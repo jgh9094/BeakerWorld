bc82aa4dab815a639a3a35f5aec5e3cca9b768d5
JoseHernandez9094 <josexavi06@gmail.com>
2019-07-22 19:34:56 -0400

    changed for debugging resource manager

--- source/native/BeakerWorld.cc
+++ source/native/BeakerWorld.cc
@@ -10,31 +10,19 @@
 ///< Experiemnt includes
 #include "../config.h"
 #include "../BeakerWorld.h"
+#include "../ResourceManager.h"
 
 int main(int argc, char* argv[])
 {
-  BeakerWorldConfig config;
+  BeakerConfig config;
   config.Read("BeakerWorld.cfg", false);
   auto args = emp::cl::ArgManager(argc, argv);
   if (args.ProcessConfigOptions(config, std::cout, "BeakerWorld.cfg", "BeakerWorld-macros.h") == false) exit(0);
   if (args.TestUnknown() == false) exit(0);  // If there are leftover args, throw an error.
 
-  std::ofstream file; 
-  file.open("timeseries.csv");
-  file << "updates,primary,apex" << std::endl;
+  std::cout << "Begining Resource Manager" << std::endl;
+  ResourceManager rm(config);
+  rm.PrintSectors();
+  std::cout << "Finished Resource Manager" << std::endl;
 
-  BeakerWorld world(config);
-
-  for (size_t ud = 0; ud < config.MAX_UPS(); ud++) {
-    world.Update();
-    std::cout <<  "***********************************\n" << std::endl;
-    std::cout << "UD: " << ud
-              << "  NumOrgs=" << world.GetNumOrgs()
-              << "  ID_MAP=" << world.GetIDSize()
-              << "  LimeOrgs=" << world.GetLime()
-              << "  RedOrgs=" << world.GetRed()
-              << std::endl;
-
-    file << world.GetUpdate() << "," << world.GetLime() << "," << world.GetRed() << std::endl;
-  }
 }
