5b176065078377b46f8dddb9b2eb4657dd3a2e0a
JoseHernandez9094 <josexavi06@gmail.com>
2019-07-22 15:25:21 -0400

    added tracking of data

--- source/native/BeakerWorld.cc
+++ source/native/BeakerWorld.cc
@@ -1,11 +1,13 @@
-// This is the main function for the NATIVE version of this project.
-
+///< C++ includes
 #include <iostream>
+#include <fstream>
 
+///<  Empirical inlcudes
 #include "base/vector.h"
 #include "config/command_line.h"
 #include "config/ArgManager.h"
 
+///< Experiemnt includes
 #include "../config.h"
 #include "../BeakerWorld.h"
 
@@ -17,12 +19,22 @@ int main(int argc, char* argv[])
   if (args.ProcessConfigOptions(config, std::cout, "BeakerWorld.cfg", "BeakerWorld-macros.h") == false) exit(0);
   if (args.TestUnknown() == false) exit(0);  // If there are leftover args, throw an error.
 
+  std::ofstream file; 
+  file.open("timeseries.csv");
+  file << "updates,primary,apex" << std::endl;
+
   BeakerWorld world(config);
 
-  for (size_t ud = 0; ud < config.MAX_GENS(); ud++) {
+  for (size_t ud = 0; ud < config.MAX_UPS(); ud++) {
     world.Update();
+    std::cout <<  "***********************************\n" << std::endl;
     std::cout << "UD: " << ud
               << "  NumOrgs=" << world.GetNumOrgs()
+              << "  ID_MAP=" << world.GetIDSize()
+              << "  LimeOrgs=" << world.GetLime()
+              << "  RedOrgs=" << world.GetRed()
               << std::endl;
+
+    file << world.GetUpdate() << "," << world.GetLime() << "," << world.GetRed() << std::endl;
   }
 }
